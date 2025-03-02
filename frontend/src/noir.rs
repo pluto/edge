a4b14b6d055f7521f1e2df356cd4e11ea898bfbf
Colin Roberts <colin@autoparallel.xyz>
2025-03-02 06:11:44 -0700

    cleanup

--- frontend/src/noir.rs
+++ frontend/src/noir.rs
@@ -66,8 +66,6 @@ impl StepCircuit<Scalar> for NoirProgram {
 
   fn circuit_index(&self) -> usize { self.index }
 
-  #[allow(clippy::too_many_lines)]
-  #[allow(clippy::too_many_lines)]
   fn synthesize<CS: ConstraintSystem<Scalar>>(
     &self,
     cs: &mut CS,
@@ -75,20 +73,10 @@ impl StepCircuit<Scalar> for NoirProgram {
     z: &[AllocatedNum<Scalar>],
   ) -> Result<(Option<AllocatedNum<Scalar>>, Vec<AllocatedNum<Scalar>>), SynthesisError> {
     trace!("Synthesizing NoirProgram with {} inputs", z.len());
-    trace!("Inner pc: {:?}", pc);
-    trace!("Circuit index: {}", self.index);
-    trace!("ABI parameters: {:?}", self.abi.parameters);
-    trace!("ABI return type: {:?}", self.abi.return_type);
-    trace!("Private parameters: {:?}", self.circuit().private_parameters);
-    trace!("Public parameters: {:?}", self.circuit().public_parameters);
-    trace!("Return values: {:?}", self.circuit().return_values);
 
-    // Create a map to track allocated variables for the cs
+    // Create variable tracker and initialize ACVM
     let mut allocated_vars: HashMap<Witness, AllocatedNum<Scalar>> = HashMap::new();
-
-    // Initialize ACVM and populate witness map from inputs
-    let mut acvm_witness_map = if let Some(inputs) = &self.witness {
-      trace!("Witness is present, initializing ACVM");
+    let acvm_witness_map = if let Some(inputs) = &self.witness {
       let mut acvm = ACVM::new(
         &StubbedBlackBoxSolver(false),
         &self.circuit().opcodes,
@@ -97,212 +85,137 @@ impl StepCircuit<Scalar> for NoirProgram {
         &[],
       );
 
-      // Convert InputMap to ACVM witness map
-      // We need to convert from Scalar to GenericFieldElement<Fr>
-      // TODO: Shouldn't clone here, but it works for now.
-      let mut inputs = inputs.private_inputs.clone();
-      inputs.insert(
+      // Prepare inputs with registers
+      let mut inputs_with_registers = inputs.private_inputs.clone();
+      inputs_with_registers.insert(
         "registers".to_string(),
         InputValue::Vec(
           z.iter()
-            .map(|z| InputValue::Field(convert_to_acir_field(z.get_value().unwrap())))
+            .filter_map(|var| var.get_value().map(|v| InputValue::Field(convert_to_acir_field(v))))
             .collect(),
         ),
       );
-      if let Ok(encoded_map) = self.abi.encode(&inputs, None) {
+
+      // Encode inputs through ABI
+      if let Ok(encoded_map) = self.abi.encode(&inputs_with_registers, None) {
         for (witness, value) in encoded_map {
-          // Convert FieldElement to GenericFieldElement<Fr>
           acvm.overwrite_witness(witness, value);
         }
       }
 
-      // Execute ACVM to get witness values
+      // Solve and get resulting witness map
       trace!("Executing ACVM solve...");
-      let status = acvm.solve();
-      trace!("ACVM solve status: {:?}", status);
-
-      let witness_map = acvm.finalize();
-      Some(witness_map)
+      acvm.solve();
+      Some(acvm.finalize())
     } else {
-      trace!("No witness provided, skipping ACVM initialization");
       None
     };
 
-    // Allocate public variables from z
+    // Allocate variables from public inputs (z)
     for (i, witness) in self.circuit().public_parameters.0.iter().enumerate() {
       if i < z.len() {
-        trace!("Allocating public input {} (witness {:?}) from z", i, witness);
         allocated_vars.insert(*witness, z[i].clone());
       }
     }
 
-    // Allocate private variables
-    for &witness in &self.circuit().private_parameters {
-      if !allocated_vars.contains_key(&witness) {
-        let value = acvm_witness_map
-          .as_ref()
-          .and_then(|map| map.get(&witness))
-          .map(|&v| convert_to_halo2_field(v));
-
-        trace!("Allocating private input (witness {:?}) with value: {:?}", witness, value);
-        let var = AllocatedNum::alloc(
-          &mut cs.namespace(|| format!("private_input_{}", witness.as_usize())),
-          || Ok(value.unwrap_or_else(Scalar::zero)),
-        )?;
-
-        allocated_vars.insert(witness, var);
-      }
-    }
-
-    // Helper function to get or create a variable for a witness
+    // Helper for getting/creating variables
     let get_var = |witness: &Witness,
-                   allocated_vars: &mut HashMap<Witness, AllocatedNum<Scalar>>,
+                   vars: &mut HashMap<Witness, AllocatedNum<Scalar>>,
                    cs: &mut CS,
-                   gate_idx: usize|
-     -> Result<Variable, SynthesisError> {
-      if let Some(var) = allocated_vars.get(witness) {
-        trace!("Using existing variable for witness {:?}", witness);
-        Ok(var.get_variable())
+                   idx: usize| {
+      if let Some(var) = vars.get(witness) {
+        Ok::<_, SynthesisError>(var.get_variable())
       } else {
-        trace!("Allocating new variable for witness {:?} in gate {}", witness, gate_idx);
-
-        // Get value from ACVM if available
         let value = acvm_witness_map
           .as_ref()
           .and_then(|map| map.get(witness))
           .map(|&v| convert_to_halo2_field(v));
 
-        let var = AllocatedNum::alloc(
-          cs.namespace(|| format!("aux_{}_w{}", gate_idx, witness.as_usize())),
-          || Ok(value.unwrap_or_else(Scalar::zero)),
-        )?;
+        let var = AllocatedNum::alloc(cs.namespace(|| format!("w{}", witness.as_usize())), || {
+          Ok(value.unwrap_or_else(Scalar::zero))
+        })?;
 
-        allocated_vars.insert(*witness, var.clone());
-        trace!("Added auxiliary witness {:?} to allocated_vars", witness);
-
+        vars.insert(*witness, var.clone());
         Ok(var.get_variable())
       }
     };
 
     // Process gates
-    trace!("Processing {} gates", self.circuit().opcodes.len());
-    for (gate_idx, opcode) in self.circuit().opcodes.iter().enumerate() {
+    for (idx, opcode) in self.circuit().opcodes.iter().enumerate() {
       if let Opcode::AssertZero(gate) = opcode {
-        // Initialize empty linear combinations for each part of our R1CS constraint
         let mut left_terms = LinearCombination::zero();
         let mut right_terms = LinearCombination::zero();
         let mut final_terms = LinearCombination::zero();
 
-        // Process multiplication terms (these form the A and B matrices in R1CS)
+        // Process multiplication terms
         for mul_term in &gate.mul_terms {
-          let coeff = convert_to_halo2_field(mul_term.0);
-          let left_var = get_var(&mul_term.1, &mut allocated_vars, cs, gate_idx)?;
-          let right_var = get_var(&mul_term.2, &mut allocated_vars, cs, gate_idx)?;
-
-          // Build Az (left terms) with coefficient
-          left_terms = left_terms + (coeff, left_var);
-          // Build Bz (right terms) with coefficient 1
+          let left_var = get_var(&mul_term.1, &mut allocated_vars, cs, idx)?;
+          let right_var = get_var(&mul_term.2, &mut allocated_vars, cs, idx)?;
+          left_terms = left_terms + (convert_to_halo2_field(mul_term.0), left_var);
           right_terms = right_terms + (Scalar::one(), right_var);
         }
 
-        // Process addition terms (these contribute to the C matrix in R1CS)
+        // Process addition terms
         for add_term in &gate.linear_combinations {
-          let coeff = convert_to_halo2_field(add_term.0);
-          let var = get_var(&add_term.1, &mut allocated_vars, cs, gate_idx)?;
-          final_terms = final_terms + (coeff, var);
+          let var = get_var(&add_term.1, &mut allocated_vars, cs, idx)?;
+          final_terms = final_terms + (convert_to_halo2_field(add_term.0), var);
         }
 
-        // Handle constant term if present
+        // Handle constant term
         if !gate.q_c.is_zero() {
-          let const_coeff = convert_to_halo2_field(gate.q_c);
-          // Negate the constant term since we're moving it to the other side of the equation
-          final_terms = final_terms - (const_coeff, Variable::new_unchecked(Index::Input(0)));
+          final_terms = final_terms
+            - (convert_to_halo2_field(gate.q_c), Variable::new_unchecked(Index::Input(0)));
         }
 
-        // Enforce the R1CS constraint: Az ∘ Bz = Cz
-        cs.enforce(
-          || format!("gate_{gate_idx}"),
-          |_| left_terms.clone(),
-          |_| right_terms.clone(),
-          |_| final_terms,
-        );
+        // Enforce constraint
+        cs.enforce(|| format!("g{}", idx), |_| left_terms, |_| right_terms, |_| final_terms);
       } else {
-        panic!("non-AssertZero gate {} of type {:?}", gate_idx, opcode);
+        panic!("non-AssertZero gate {} of type {:?}", idx, opcode);
       }
     }
 
     // Prepare return values
-    trace!("Preparing return values");
     let mut return_values = vec![];
-
-    // Ensure all return witnesses have allocated variables
-    for (i, ret) in self.circuit().return_values.0.iter().enumerate() {
+    for ret in &self.circuit().return_values.0 {
+      // Ensure return witness has an allocated variable
       if !allocated_vars.contains_key(ret) {
-        trace!("Return value {} (witness {:?}) not yet allocated, creating", i, ret);
-
-        // Get value from ACVM if available
         let value = acvm_witness_map
           .as_ref()
           .and_then(|map| map.get(ret))
           .map(|&v| convert_to_halo2_field(v));
 
-        let var = AllocatedNum::alloc(&mut cs.namespace(|| format!("return_value_{}", i)), || {
+        let var = AllocatedNum::alloc(cs.namespace(|| format!("ret{}", ret.as_usize())), || {
           Ok(value.unwrap_or_else(Scalar::zero))
         })?;
 
         allocated_vars.insert(*ret, var);
       }
-
-      trace!("Adding return value {} (witness {:?}) to results", i, ret);
       return_values.push(allocated_vars[ret].clone());
     }
 
-    // Extract return structure from ABI
-    if let Some(return_type) = &self.abi.return_type {
-      if let AbiType::Struct { fields, .. } = &return_type.abi_type {
-        // Find the registers field in the struct
-        let (registers_length, next_pc_index) = fields
+    // Extract return structure (registers and next_pc)
+    if let Some(noirc_abi::AbiReturnType { abi_type: AbiType::Struct { fields, .. }, .. }) =
+      &self.abi.return_type
+    {
+      let registers_length = fields
         .iter()
         .find(|(name, _)| name == "registers")
         .map(|(_, typ)| match typ {
-          AbiType::Array { length, .. } => (*length as usize, *length as usize), // next_pc follows registers
-          _ => panic!("Expected registers to be an array type, found {:?}", typ),
+          AbiType::Array { length, .. } => *length as usize,
+          _ => panic!("Expected registers to be an array type"),
         })
-        .unwrap_or_else(|| panic!("Expected 'registers' field in return struct"));
+        .unwrap_or_else(|| panic!("Missing 'registers' field"));
 
-        trace!(
-          "Return struct has registers_length={}, next_pc_index={}",
-          registers_length,
-          next_pc_index
-        );
+      let next_pc_index = registers_length;
 
-        if next_pc_index < return_values.len() {
-          // Extract next_pc and registers
-          let next_pc = Some(return_values[next_pc_index].clone());
-          let registers = return_values[..registers_length].to_vec();
-
-          trace!(
-            "Returning next_pc at index {} and {} register values",
-            next_pc_index,
-            registers.len()
-          );
-          return Ok((next_pc, registers));
-        } else {
-          trace!(
-            "ERROR: next_pc_index {} out of bounds for return_values length {}",
-            next_pc_index,
-            return_values.len()
-          );
-          return Err(SynthesisError::Unsatisfiable);
-        }
-      } else {
-        trace!("Return type is not a struct: {:?}", return_type.abi_type);
-        return Err(SynthesisError::Unsatisfiable);
+      if next_pc_index < return_values.len() {
+        let next_pc = Some(return_values[next_pc_index].clone());
+        let registers = return_values[..registers_length].to_vec();
+        return Ok((next_pc, registers));
       }
-    } else {
-      trace!("No return type specified");
-      return Err(SynthesisError::Unsatisfiable);
     }
+
+    Err(SynthesisError::Unsatisfiable)
   }
 }
 
