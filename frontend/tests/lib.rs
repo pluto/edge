2974e16206afde374d2a507046492a133be896db
Colin Roberts <colin@autoparallel.xyz>
2025-03-11 17:19:21 -0600

    refactor: `nivc` package only
    This is ONLY for release of the `nivc` package. DO NOT merge this to main!

--- frontend/tests/lib.rs
+++
@@ -1,5 +0,0 @@
-use edge_frontend::demo::*;
-use tracing_test::traced_test;
-
-mod end_to_end;
-mod ivc;
--- prover/src/bellpepper/solver.rs
+++
@@ -1,9 +0,0 @@
-//! Support for generating R1CS witness using bellpepper.
-
-use bellpepper::util_cs::witness_cs::WitnessCS;
-
-use crate::traits::Engine;
-
-/// A `ConstraintSystem` which calculates witness values for a concrete instance
-/// of an R1CS circuit.
-pub type SatisfyingAssignment<E> = WitnessCS<<E as Engine>::Scalar>;
